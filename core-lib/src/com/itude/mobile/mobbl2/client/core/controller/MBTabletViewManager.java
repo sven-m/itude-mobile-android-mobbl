d9a073d6f12517741ab05450c2ca8ed14ee5894b
Cristan Meijer <c.meijer@itude.com>
2012-05-29 14:50:55 +0000

    Reverted my last commit

--- core-lib/src/com/itude/mobile/mobbl2/client/core/controller/MBTabletViewManager.java
+++ core-lib/src/com/itude/mobile/mobbl2/client/core/controller/MBTabletViewManager.java
@@ -361,7 +361,7 @@ public class MBTabletViewManager extends MBViewManager
 
             for (MBDomainValidatorDefinition domDef : domainDef.getDomainValidators())
             {
-              spinnerAdapter.add(MBLocalizationService.getInstance().getTextForKey(domDef.getTitle()));
+              spinnerAdapter.add(domDef.getTitle());
             }
 
             Drawable drawable = MBResourceService.getInstance().getImageByID("tab-spinner-leaf");
