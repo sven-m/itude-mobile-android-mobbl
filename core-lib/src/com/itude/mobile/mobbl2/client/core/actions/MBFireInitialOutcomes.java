5c029d1a9309e3baa5513c8b38b62b786aeca696
Pjotter Tommassen <p.tommassen@itude.com>
2012-11-05 18:14:54 +0100

    - fix for applications making network connections in the initial outcome of the first opened dialog.. :')

--- core-lib/src/com/itude/mobile/mobbl2/client/core/actions/MBFireInitialOutcomes.java
+++ core-lib/src/com/itude/mobile/mobbl2/client/core/actions/MBFireInitialOutcomes.java
@@ -5,9 +5,11 @@ import java.util.List;
 import com.itude.mobile.mobbl2.client.core.controller.MBAction;
 import com.itude.mobile.mobbl2.client.core.controller.MBApplicationController;
 import com.itude.mobile.mobbl2.client.core.controller.MBOutcome;
+import com.itude.mobile.mobbl2.client.core.controller.MBViewManager;
 import com.itude.mobile.mobbl2.client.core.model.MBDocument;
 import com.itude.mobile.mobbl2.client.core.model.MBElement;
 import com.itude.mobile.mobbl2.client.core.services.MBDataManagerService;
+import com.itude.mobile.mobbl2.client.core.util.StringUtilities;
 
 public class MBFireInitialOutcomes implements MBAction
 {
@@ -16,21 +18,41 @@ public class MBFireInitialOutcomes implements MBAction
     return "InitialOutcomes";
   }
 
+  protected boolean isFirstDialogSynchronized()
+  {
+    return true;
+  }
+
   @Override
   public MBOutcome execute(MBDocument document, String path)
   {
     MBDocument initialOutcomes = MBDataManagerService.getInstance().loadDocument(getDocumentName());
     @SuppressWarnings("unchecked")
     List<MBElement> elements = (List<MBElement>) initialOutcomes.getValueForPath("/Outcome");
+    boolean first = true;
     for (MBElement element : elements)
     {
-      MBOutcome oc = new MBOutcome();
-      oc.setOutcomeName((String) element.getValueForPath("@action"));
-      oc.setDialogName((String) element.getValueForPath("@dialog"));
-      oc.setNoBackgroundProcessing(true);
-      oc.setTransferDocument(false);
+      String action = element.getValueForAttribute("action");
+      String dialog = element.getValueForAttribute("dialog");
+      if (StringUtilities.isNotBlank(action))
+      {
+        MBOutcome oc = new MBOutcome();
+        oc.setOutcomeName(action);
+        oc.setDialogName(dialog);
+        oc.setNoBackgroundProcessing(true);
+        oc.setTransferDocument(false);
 
-      MBApplicationController.getInstance().getOutcomeHandler().handleOutcomeSynchronously(oc, false);
+        if (first && isFirstDialogSynchronized())
+        {
+          MBApplicationController.getInstance().getOutcomeHandler().handleOutcomeSynchronously(oc, false);
+          first = false;
+        }
+        else
+        {
+          MBApplicationController.getInstance().handleOutcome(oc);
+        }
+      }
+      MBViewManager.getInstance().addSortedDialogName(dialog);
     }
 
     return null;
