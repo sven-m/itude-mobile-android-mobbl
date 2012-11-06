22d3450e8c8985160b65dd5ca1eaf95807c330ec
Pjotter Tommassen <p.tommassen@itude.com>
2012-11-06 14:04:12 +0100

    - also use a registry for view controller mappings

--- core-lib/src/com/itude/mobile/mobbl2/client/core/controller/MBApplicationFactory.java
+++ core-lib/src/com/itude/mobile/mobbl2/client/core/controller/MBApplicationFactory.java
@@ -21,10 +21,12 @@ public class MBApplicationFactory
 {
   private static MBApplicationFactory _instance = null;
   private final ActionMappings        _actions;
+  private final ControllerMappings    _controllers;
 
   public MBApplicationFactory()
   {
     _actions = new ActionMappings(getActionRegistry());
+    _controllers = new ControllerMappings(getControllerRegistry());
   }
 
   public static MBApplicationFactory getInstance()
@@ -39,11 +41,6 @@ public class MBApplicationFactory
   public static void setInstance(MBApplicationFactory factory)
   {
     _instance = factory;
-  }
-
-  public MBBasicViewController createViewController(MBPage page)
-  {
-    return new MBBasicViewController();
   }
 
   public MBPage createPage(MBPageDefinition definition, MBDocument document, String rootPath, MBViewManager.MBViewState viewState)
@@ -64,7 +61,7 @@ public class MBApplicationFactory
 
   public MBBasicViewController createFragment(String pageName)
   {
-    return new MBBasicViewController();
+    return _controllers.createController(pageName);
   }
 
   public MBDialogController createDialogController()
@@ -78,6 +75,11 @@ public class MBApplicationFactory
   }
 
   protected ActionMappings.Registry getActionRegistry()
+  {
+    return null;
+  }
+
+  protected ControllerMappings.Registry getControllerRegistry()
   {
     return null;
   }
@@ -100,11 +102,6 @@ public class MBApplicationFactory
       }
 
       protected abstract void registerMappings();
-
-      Map<String, Class<? extends MBAction>> getActions()
-      {
-        return actions;
-      }
     }
 
     private final Map<String, Class<? extends MBAction>> _actions;
@@ -129,6 +126,68 @@ public class MBApplicationFactory
       }
 
       else throw new MBException("Action " + actionClassName + " not found");
+    }
+  }
+
+  public static class ControllerMappings
+  {
+    public static abstract class Registry
+    {
+      private Map<String, Class<? extends MBBasicViewController>> controllers;
+      private Class<? extends MBBasicViewController>              defaultController;
+
+      protected Registry()
+      {
+        controllers = new HashMap<String, Class<? extends MBBasicViewController>>();
+        defaultController = MBBasicViewController.class;
+        registerMappings();
+      }
+
+      protected void registerController(String pageName, Class<? extends MBBasicViewController> controller)
+      {
+        controllers.put(pageName, controller);
+      }
+
+      protected void setDefaultController(Class<? extends MBBasicViewController> defaultController)
+      {
+        this.defaultController = defaultController;
+      }
+
+      protected abstract void registerMappings();
+    }
+
+    private final Map<String, Class<? extends MBBasicViewController>> _controllers;
+    private final Class<? extends MBBasicViewController>              _defaultController;
+
+    protected ControllerMappings(Registry registry)
+    {
+      if (registry != null) {
+        _controllers = Collections.unmodifiableMap(registry.controllers);
+        _defaultController = registry.defaultController;
+      }
+      else {
+        _controllers = Collections.emptyMap();
+        _defaultController = MBBasicViewController.class;ff
+      }
+
+      
+    }
+
+    MBBasicViewController createController(String pageName)
+    {
+      Class<? extends MBBasicViewController> controller = _controllers.get(pageName);
+      if (controller == null) controller = _defaultController;
+
+      if (controller != null) try
+      {
+        return controller.newInstance();
+      }
+      catch (Exception e)
+      {
+        throw new MBException("Error instantiating " + controller.getSimpleName(), e);
+      }
+
+      else throw new MBException("Controller for page  " + pageName + " not found");
     }
   }
 
