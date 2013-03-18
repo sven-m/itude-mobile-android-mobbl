884087f394cd5473de033fb18d9c302e6c3a2489
Pjotter Tommassen <p.tommassen@itude.com>
2013-03-18 10:33:29 +0100

    - make sure applications work without the GET_TASKS permission

--- core-lib/src/com/itude/mobile/mobbl2/client/core/controller/helpers/MBActivityHelper.java
+++ core-lib/src/com/itude/mobile/mobbl2/client/core/controller/helpers/MBActivityHelper.java
@@ -2,13 +2,20 @@ package com.itude.mobile.mobbl2.client.core.controller.helpers;
 
 import java.util.List;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 public class MBActivityHelper
 {
+
+  private MBActivityHelper()
+  {
+
+  }
 
   /**
   * Checks if the application is in the background (i.e behind another application's Activity).
@@ -18,6 +25,8 @@ public class MBActivityHelper
   */
   public static boolean isApplicationBroughtToBackground(final Context context)
   {
+    if (context.checkCallingOrSelfPermission(Manifest.permission.GET_TASKS) == PackageManager.PERMISSION_DENIED) return false;
+
     ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
     List<RunningTaskInfo> tasks = am.getRunningTasks(1);
     if (!tasks.isEmpty())
