565d17fa5025739de6b1952db4bf611fcca378e5
w.elsinga@itude.com <w.elsinga@itude.com>
2013-07-12 12:06:04 +0200

    Minnor changes

--- core-lib/src/com/itude/mobile/mobbl2/client/core/services/datamanager/util/MBRequestUtil.java
+++ core-lib/src/com/itude/mobile/mobbl2/client/core/services/datamanager/util/MBRequestUtil.java
@@ -6,20 +6,51 @@ import com.itude.mobile.mobbl2.client.core.util.Constants;
 
 public final class MBRequestUtil
 {
-  public static void setRequestParameter(boolean value, String key, MBDocument doc)
+
+  private MBRequestUtil()
   {
-    setRequestParameter(value ? Constants.C_TRUE : Constants.C_FALSE, key, doc);
   }
 
   /**
-   * @param root root element
+   * @param value boolean value
+   * @param key key
+   * @param doc {@link MBDocument}
+   */
+  public static void setRequestParameter(boolean value, String key, MBDocument doc)
+  {
+    setRequestParameter("Operation", value, key, doc);
+  }
+
+  /**
+   * @param rootElement root element
+   * @param value boolean value
+   * @param key key
+   * @param doc {@link MBDocument}
+   */
+  public static void setRequestParameter(String rootElement, boolean value, String key, MBDocument doc)
+  {
+    setRequestParameter(rootElement, value ? Constants.C_TRUE : Constants.C_FALSE, key, doc);
+  }
+
+  /**
    * @param value value
    * @param key key
    * @param doc {@link MBDocument}
    */
   public static void setRequestParameter(String value, String key, MBDocument doc)
   {
-    MBElement request = (MBElement) doc.getValueForPath("Operation[0]");
+    setRequestParameter("Operation", value, key, doc);
+  }
+
+  /**
+   * @param rootElement root element
+   * @param value value
+   * @param key key
+   * @param doc {@link MBDocument}
+   */
+  public static void setRequestParameter(String rootElement, String value, String key, MBDocument doc)
+  {
+    MBElement request = (MBElement) doc.getValueForPath(rootElement + "[0]");
     MBElement parameter = request.createElement("Parameter");
     parameter.setAttributeValue(key, "key");
     parameter.setAttributeValue(value, "value");
