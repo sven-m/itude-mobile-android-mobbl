e56aace56384603f59be865c1e3e8e16929768d4
w.elsinga@itude.com <w.elsinga@itude.com>
2013-10-15 12:11:09 +0200

    Removed core-app. We use kitchensink for a demo app and core-test for testing the library.

--- core-test/src/com/itude/mobile/mobbl2/client/core/tests/AllTests.java
+++ core-test/src/com/itude/mobile/mobbl2/client/core/tests/AllTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.itude.mobile.mobbl2.client.core;
+package com.itude.mobile.mobbl2.client.core.tests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
