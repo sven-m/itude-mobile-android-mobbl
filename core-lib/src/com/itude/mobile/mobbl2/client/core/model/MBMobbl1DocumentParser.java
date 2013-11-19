832f549b6645e870890412abd17085f9d7fde978
w.elsinga@gmail.com <w.elsinga@gmail.com>
2013-11-19 11:47:09 +0100

    Changes license (https://mobiledev.itude.com/jira/browse/MOBBL-938)

--- core-lib/src/com/itude/mobile/mobbl2/client/core/model/MBMobbl1DocumentParser.java
+++ core-lib/src/com/itude/mobile/mobbl2/client/core/model/MBMobbl1DocumentParser.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright ItudeMobile.
+ * (C) Copyright Itude Mobile B.V., The Netherlands
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
