e8286a911883dbad932ed21b3c2fb0d4b8f50059
Kevin van Mastrigt <k.vanmastrigt@itude.com>
2015-05-19 17:29:20 +0200

    Tests for MBDocument

---
+++ library/src/androidTest/java/com/itude/mobile/mobbl/core/model/MBDocumentTest.java
@@ -0,0 +1,136 @@
+package com.itude.mobile.mobbl.core.model;
+
+import android.os.Parcel;
+
+import com.itude.mobile.mobbl.core.configuration.mvc.MBDocumentDefinition;
+import com.itude.mobile.mobbl.core.configuration.mvc.MBMvcConfigurationParser;
+import com.itude.mobile.mobbl.core.services.MBDataManagerService;
+import com.itude.mobile.mobbl.core.services.MBMetadataService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Kevin on 5/19/15.
+ */
+public class MBDocumentTest extends MBDocumentAbstractTest {
+
+    MBDocument emptyDoc, booksDoc, booksDoc2;
+
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        emptyDoc = new MBDocument();
+
+        MBDocumentDefinition docDef = getConfig().getDefinitionForDocumentName("Books");
+        assertNotNull(docDef);
+
+        booksDoc = MBDocumentFactory.getInstance().getDocumentWithData(getXmlDocumentData(), MBDocumentFactory.PARSER_XML, docDef);
+        assertNotNull(booksDoc);
+
+        MBDocumentDefinition docDef2 = getConfig().getDefinitionForDocumentName("Books");
+        assertNotNull(docDef2);
+
+        booksDoc2 = MBDocumentFactory.getInstance().getDocumentWithData(getXmlDocumentData(), MBDocumentFactory.PARSER_XML, docDef2);
+        assertNotNull(booksDoc2);
+    }
+
+
+    /**
+     * simple constructor test, just to improve coverage
+     */
+    public void testMBDocument()
+    {
+        MBDocument doc = new MBDocument();
+        assertNotNull(doc);
+        assertNotNull(doc.getSharedContext());
+    }
+
+    /**
+     * Test if the sharedContext is not null in an empty and
+     * in a filled document
+     */
+    public void testGetSharedContext()
+    {
+        assertNotNull(emptyDoc.getSharedContext());
+        assertNotNull(booksDoc.getSharedContext());
+    }
+
+    /**
+     * Test if setSharedContext correctly sets the sharedcontext
+     */
+    public void testSetSharedContext()
+    {
+        HashMap<String, MBDocument> sharedContext = new HashMap<String, MBDocument>();
+        sharedContext.put("test", emptyDoc);
+        booksDoc.setSharedContext(sharedContext);
+        assertEquals(booksDoc.getSharedContext(), sharedContext);
+    }
+
+    /**
+     * Test if the getter and setter of argumentsUsed work correctly
+     */
+    public void testGetAndSetArgumentsUsed()
+    {
+        booksDoc.setArgumentsUsed(emptyDoc);
+        MBDocument arg = booksDoc.getArgumentsUsed();
+        assertEquals(emptyDoc, arg);
+    }
+
+    /**
+     * Test if a document is correctly assigned
+     */
+    public void testAssignToDocument()
+    {
+        booksDoc2.assignToDocument(booksDoc);
+        assertEquals(booksDoc2.getElements().size(), booksDoc.getElements().size());
+    }
+
+    public void testLoadFreshCopy()
+    {
+
+        // TODO: Figure out why config can't be parsed.
+
+        //MBDocument copy = booksDoc.loadFreshCopy();
+        //assertEquals(copy.getElements(), booksDoc.getElements());
+
+        //booksDoc.setArgumentsUsed(emptyDoc);
+        //copy = booksDoc.loadFreshCopy();
+        //assertEquals(copy.getElements(), booksDoc.getElements());
+
+    }
+
+    public void testReload()
+    {
+        // Same problem as with testLoadFreshCopy()
+    }
+
+    /**
+     * Test if the getDocument function returns the correct instance
+     */
+    public void testGetDocument()
+    {
+        MBDocument doc = booksDoc.getDocument();
+        assertEquals(doc, booksDoc);
+    }
+
+    /**
+     * Test if writing a document to a parcel and creating a document from a parcel work
+     */
+    public void testParcel()
+    {
+        Parcel p = Parcel.obtain();
+        booksDoc.writeToParcel(p, 0);
+        assertNotNull(p);
+
+        MBDocument doc = booksDoc.CREATOR.createFromParcel(p);
+        assertNotNull(doc);
+    }
+
+
+
+
+
+}
