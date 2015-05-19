36643b92f1ed095c003f40c82e3584b596a875ee
Kevin van Mastrigt <k.vanmastrigt@itude.com>
2015-05-19 13:46:16 +0200

    Het starten van de activity wil echt niet lukken. Comments aan huidige tests toegevoegd.

--- library/src/androidTest/java/com/itude/mobile/mobbl/core/controller/MBApplicationControllerTest.java
+++ library/src/androidTest/java/com/itude/mobile/mobbl/core/controller/MBApplicationControllerTest.java
@@ -1,38 +1,57 @@
 package com.itude.mobile.mobbl.core.controller;
-
 import android.test.ApplicationTestCase;
-import android.view.ViewManager;
 
-import com.itude.mobile.mobbl.core.MBApplicationCore;
-import com.itude.mobile.mobbl.core.services.MBMetadataService;
-
-import junit.framework.TestCase;
-
-public class MBApplicationControllerTest extends ApplicationTestCase<MBApplicationCore> {
+public class MBApplicationControllerTest extends ApplicationTestCase<MBApplicationController> {
 
     private MBApplicationController app;
 
     public MBApplicationControllerTest()
     {
-        super(MBApplicationCore.class);
+        super(MBApplicationController.class);
     }
 
+    /**
+     * Get the application instance
+     * @throws Exception
+     */
     @Override
     public void setUp() throws Exception {
         app = MBApplicationController.getInstance();
     }
 
+    /**
+     * Test if the app is not null
+     * @throws Exception
+     */
     public void testGetInstance() throws Exception {
         assertNotNull(app);
     }
 
-//    public void testGetViewManager() throws Exception {
-//        MBViewManager viewManager = this.app.getViewManager();
-//        assertNotNull(viewManager);
-//    }
+    /**
+     * Test if getViewManager returns the correct instance
+     * @throws Exception
+     */
+    public void testGetViewManager() throws Exception {
+        MBViewManager viewManager = this.app.getViewManager();
+        assertEquals(viewManager, MBViewManager.getInstance());
+    }
 
+    /**
+     * Test if currentinstance equals app
+     * @throws Exception
+     */
     public void testCurrentInstance() throws Exception {
         MBApplicationController curInstance = this.app.currentInstance();
         assertEquals(app, curInstance);
     }
+
+    /**
+     * If this test fails, something is really wrong
+     */
+    public void testShouldHandleOutcome()
+    {
+        boolean result = app.shouldHandleOutcome(new MBOutcome());
+        assertTrue(result);
+    }
 }
+
