6c7eb2f58a93a695d3dfad7f5012edb7bbaa4e4f
Kevin van Mastrigt <k.vanmastrigt@itude.com>
2015-06-18 16:06:51 +0200

    Made tests more independent, and added new tests.

--- library/src/androidTest/java/com/itude/mobile/mobbl/core/model/MBDocumentTest.java
+++ library/src/androidTest/java/com/itude/mobile/mobbl/core/model/MBDocumentTest.java
@@ -3,11 +3,15 @@ package com.itude.mobile.mobbl.core.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.itude.mobile.mobbl.core.configuration.endpoints.MBEndPointDefinition;
 import com.itude.mobile.mobbl.core.configuration.mvc.MBDocumentDefinition;
 import com.itude.mobile.mobbl.core.configuration.mvc.MBElementDefinition;
 import com.itude.mobile.mobbl.core.configuration.mvc.MBMvcConfigurationParser;
 import com.itude.mobile.mobbl.core.services.MBDataManagerService;
 import com.itude.mobile.mobbl.core.services.MBMetadataService;
+import com.itude.mobile.mobbl.core.services.datamanager.MBDataHandler;
+import com.itude.mobile.mobbl.core.services.operation.MBDocumentOperationDelegate;
+import com.itude.mobile.mobbl.core.util.MBConstants;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -20,115 +24,93 @@ import java.util.Map;
  */
 public class MBDocumentTest extends MBDocumentAbstractTest {
 
-    MBDocument emptyDoc, booksDoc, booksDoc2;
+    MBDocument emptyDoc;
 
     @Override
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         super.setUp();
-        emptyDoc = new MBDocument();
-
-        MBDocumentDefinition docDef = getConfig().getDefinitionForDocumentName("Books");
-        assertNotNull(docDef);
-
-        booksDoc = MBDocumentFactory.getInstance().getDocumentWithData(getXmlDocumentData(), MBDocumentFactory.PARSER_XML, docDef);
-        assertNotNull(booksDoc);
-
-        MBDocumentDefinition docDef2 = getConfig().getDefinitionForDocumentName("Books");
-        assertNotNull(docDef2);
-
-        booksDoc2 = MBDocumentFactory.getInstance().getDocumentWithData(getXmlDocumentData(), MBDocumentFactory.PARSER_XML, docDef2);
-        assertNotNull(booksDoc2);
-    }
-
-
-    /**
-     * simple constructor test, just to improve coverage
-     */
-    public void testMBDocument()
-    {
-        MBDocument doc = new MBDocument();
-        assertNotNull(doc);
-        assertNotNull(doc.getSharedContext());
+        MBDataManagerService dataManagerService = new MockDataManagerService();
+        emptyDoc = dataManagerService.createDocument("testDocument");
     }
 
     /**
-     * Test if the sharedContext is not null in an empty and
-     * in a filled document
+     * Test the getter and setter of the sharedcontext
      */
-    public void testGetSharedContext()
-    {
-        assertNotNull(emptyDoc.getSharedContext());
-        assertNotNull(booksDoc.getSharedContext());
-    }
-
-    /**
-     * Test if setSharedContext correctly sets the sharedcontext
-     */
-    public void testSetSharedContext()
-    {
+    public void testSetSharedContext() {
         HashMap<String, MBDocument> sharedContext = new HashMap<String, MBDocument>();
         sharedContext.put("test", emptyDoc);
-        booksDoc.setSharedContext(sharedContext);
-        assertEquals(booksDoc.getSharedContext(), sharedContext);
+        emptyDoc.setSharedContext(sharedContext);
+        assertEquals(emptyDoc.getSharedContext(), sharedContext);
     }
 
     /**
      * Test if the getter and setter of argumentsUsed work correctly
      */
-    public void testGetAndSetArgumentsUsed()
-    {
-        booksDoc.setArgumentsUsed(emptyDoc);
-        MBDocument arg = booksDoc.getArgumentsUsed();
+    public void testGetAndSetArgumentsUsed() {
+        emptyDoc.setArgumentsUsed(emptyDoc);
+        MBDocument arg = emptyDoc.getArgumentsUsed();
         assertEquals(emptyDoc, arg);
     }
 
     /**
      * Test if a document is correctly assigned
      */
-    public void testAssignToDocument()
-    {
-        booksDoc2.assignToDocument(booksDoc);
-        assertEquals(booksDoc2.getElements().size(), booksDoc.getElements().size());
+    public void testAssignToDocument() {
+        emptyDoc.assignToDocument(emptyDoc);
+        assertEquals(emptyDoc.getElements().size(), emptyDoc.getElements().size());
     }
 
-    public void testLoadFreshCopy()
-    {
+    /**
+     * Test the loadFreshCopy function, using a mocked dataHandler.
+     */
+    public void testLoadFreshCopy() {
 
-        // TODO: Figure out why config can't be parsed.
+        MBDocument copy = emptyDoc.loadFreshCopy();
+        assertEquals(copy.getName(), emptyDoc.getName());
 
-        //MBDocument copy = booksDoc.loadFreshCopy();
-        //assertEquals(copy.getElements(), booksDoc.getElements());
-
-        //booksDoc.setArgumentsUsed(emptyDoc);
-        //copy = booksDoc.loadFreshCopy();
-        //assertEquals(copy.getElements(), booksDoc.getElements());
+        emptyDoc.setArgumentsUsed(emptyDoc);
+        copy = emptyDoc.loadFreshCopy();
+        assertEquals(copy.getName(), emptyDoc.getName());
+        assertEquals(copy.getArgumentsUsed().getName(), emptyDoc.getArgumentsUsed().getName());
 
     }
 
-    public void testReload()
-    {
-        // Same problem as with testLoadFreshCopy()
+    /**
+     * Test the reload function of a document, using a mocked dataHandler.
+     */
+    public void testReload() {
+        String name = emptyDoc.getName();
+        emptyDoc.reload();
+        assertEquals(emptyDoc.getName(), name);
+
+        emptyDoc.setArgumentsUsed(emptyDoc);
+        emptyDoc.reload();
+        assertEquals(emptyDoc.getArgumentsUsed().getName(), name);
     }
+
+    public void testDescribeContents() {
+        int contents = emptyDoc.describeContents();
+        assertEquals(contents, MBConstants.C_PARCELABLE_TYPE_DOCUMENT);
+    }
+
+
 
     /**
      * Test if the getDocument function returns the correct instance
      */
-    public void testGetDocument()
-    {
-        MBDocument doc = booksDoc.getDocument();
-        assertEquals(doc, booksDoc);
+    public void testGetDocument() {
+        MBDocument doc = emptyDoc.getDocument();
+        assertEquals(doc, emptyDoc);
     }
 
     /**
      * Test if writing a document to a parcel and creating a document from a parcel work
      */
-    public void testParcel()
-    {
+    public void testParcel() {
         // Obtain an empty parcel
         Parcel p = Parcel.obtain();
         // Write 'booksdoc' to the parcel
-        booksDoc.writeToParcel(p, 0);
+        emptyDoc.writeToParcel(p, 0);
         // Check if the parcel is not null
         assertNotNull(p);
 
@@ -140,8 +122,112 @@ public class MBDocumentTest extends MBDocumentAbstractTest {
 
     }
 
+    private class MockDataManagerService extends MBDataManagerService {
 
+        public MockDataManagerService() {
+        }
 
+        @Override
+        protected void registerDataHandlers() {
+        }
+
+        @Override
+        public MBDocument createDocument(String documentName) {
+            MBDocumentDefinition def = new MBDocumentDefinition();
+            def.setName(documentName);
+            MBDocument doc =  new MBDocument(def, this);
+            doc.createElement("testElement").setBodyText("test");
+            return doc;
+        }
+
+        @Override
+        public MBDocument loadDocument(String documentName) {
+            return createDocument(documentName);
+        }
+
+        @Override
+        public MBDocument loadFreshDocument(String documentName) {
+            return createDocument(documentName);
+        }
+
+        @Override
+        public MBDocument loadDocument(String documentName, MBDocument doc) {
+            return createDocument(documentName);
+        }
+
+        @Override
+        public MBDocument loadFreshDocument(String documentName, MBDocument doc) {
+            MBDocument newDoc = createDocument(documentName);
+            newDoc.setArgumentsUsed(doc);
+            return newDoc;
+        }
+
+        @Override
+        public void loadDocument(String documentName, MBDocumentOperationDelegate delegate) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadFreshDocument(String documentName, MBDocumentOperationDelegate delegate) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadDocument(String documentName, MBDocument args, MBDocumentOperationDelegate delegate) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadFreshDocument(String documentName, MBDocument args, MBDocumentOperationDelegate delegate) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadDocument(String documentName, MBDocument args, MBDocumentOperationDelegate delegate, MBEndPointDefinition endPointDefinition) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadFreshDocument(String documentName, MBDocument args, MBDocumentOperationDelegate delegate, MBEndPointDefinition endPointDefinition) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadDocument(String documentName, MBDocument args, MBDocumentOperationDelegate delegate, String documentParser) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void loadFreshDocument(String documentName, MBDocument args, MBDocumentOperationDelegate delegate, String documentParser) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void storeDocument(MBDocument document) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void storeDocument(MBDocument document, MBDocumentOperationDelegate delegate, Object resultSelector, Object errorSelector) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void registerDataHandler(MBDataHandler handler, String name) {
+
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void registerOperationListener(String docName, OperationListener listener) {
+            throw new RuntimeException("Not Implemented.");
+        }
+
+        @Override
+        public void unregisterOperationListener(String docName, OperationListener listener) {
+            throw new RuntimeException("Not Implemented.");
+        }
+    }
 
 
 }
